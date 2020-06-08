d8b85f23f02109b58bcec8928055d167be780065
devYSK <kim206gh@naver.com>
2020-06-08 22:58:06 +0900

    appspec.yml 오타 수정

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
 ABSPATH=$(readlink -f $0)
-ABSDIR=${dirname $ABSPATH}
+ABSDIR=$(dirname $ABSPATH)
 source ${ABSDIR}/profile.sh
 
 IDLE_PORT=$(find_idle_port)
 
-if [ -z ${IDLE_PID}]
+echo "> $IDLE_PORT 에서 구동중인 애플리케이션 pid 확인"
+IDLE_PID=$(lsof -ti tcp:${IDLE_PORT})
+
+if [ -z ${IDLE_PID} ]
 then
   echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
@@ -14,4 +17,3 @@ else
   kill -15 ${IDLE_PID}
   sleep 5
 fi
-
